968287aa9132bb7f2adb18712cea87608aec0948
Ra}{ul Unecha <runecha08@gmail.com>
2024-11-07 11:11:27 +0530

    ull

---
+++ RDSmsql/main.tf
@@ -0,0 +1,14 @@
+resource "aws_db_instance" "this_rds" {
+  allocated_storage    = var.this_list[0]                
+  engine               = var.this_list[1]                
+  engine_version       = var.this_list[2]                     
+  instance_class       = var.this_list[3]           
+  db_name              = var.this_list[4]           
+  username             = var.this_list[5]                   
+  password             = var.this_list[6]               
+  parameter_group_name = var.this_list[7]        
+  skip_final_snapshot  = var.this_list[8]                     
+  publicly_accessible  = var.this_list[9]                      
+  vpc_security_group_ids = [var.this_list[10]]  
+  multi_az             = var.this_list[11]                     
+}
---
+++ RDSmsql/variable.tf
@@ -0,0 +1,5 @@
+variable "this_list" {
+    type = list 
+    default = ["20" , "mysql" , "8.0" , "db.t3.micro" , "mydatabase_mysql_1" , "admin" , "password123" , "default.mysql8.0" , "true" , "true" , "sg-0534de5a41b183c8f" , "false"]
+     
+}
