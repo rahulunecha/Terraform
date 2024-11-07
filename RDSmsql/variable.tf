0d4a82b7485c565418c305ee6c7e9fef30549ebf
Ra}{ul Unecha <runecha08@gmail.com>
2024-11-07 20:30:13 +0530

    vv

--- RDSmsql/variable.tf
+++ RDSmsql/variable.tf
@@ -1,5 +1,5 @@
 variable "this_list" {
     type = list 
-    default = ["20" , "mysql" , "8.0" , "db.t3.micro" , "mydatabase_mysql_1" , "admin" , "password123" , "default.mysql8.0" , "true" , "true" , "sg-0534de5a41b183c8f" , "false"]
+    default = ["20" , "mysql" , "8.0" , "db.t4g.micro" , "mydatabase_mysql_1" , "admin" , "password123" , "default.mysql8.0" , "true" , "true" , "sg-0f43f4aa1dbd5c852" , "false"]
      
 }
