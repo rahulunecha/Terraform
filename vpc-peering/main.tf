9d04b3bdc1d0862aa8256fc71c40f0a39066a060
Ra}{ul Unecha <runecha08@gmail.com>
2024-11-08 12:20:51 +0530

    d

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -2,25 +2,61 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc_peering_connection" "peer" {
-  vpc_id        = var.vpc_id
-  peer_vpc_id   = var.peer_vpc_id
-  peer_owner_id = var.peer_owner_id  # Optional if both VPCs are in the same account
-  auto_accept   = var.auto_accept
-
+# Create VPC 1
+resource "aws_vpc" "vpc1" {
+  cidr_block = var.vpc1_cidr
   tags = {
-    Name = var.peering_name
+    Name = "VPC 1"
   }
 }
 
-resource "aws_route" "route_to_peer_vpc1" {
-  route_table_id            = var.route_table_id_vpc1
-  destination_cidr_block    = var.peer_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+# Create VPC 2
+resource "aws_vpc" "vpc2" {
+  cidr_block = var.vpc2_cidr
+  tags = {
+    Name = "VPC 2"
+  }
 }
 
-resource "aws_route" "route_to_peer_vpc2" {
-  route_table_id            = var.route_table_id_vpc2
-  destination_cidr_block    = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+# Create Route Table for VPC 1
+resource "aws_route_table" "rt_vpc1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "Route Table for VPC 1"
+  }
+}
+
+# Create Route Table for VPC 2
+resource "aws_route_table" "rt_vpc2" {
+  vpc_id = aws_vpc.vpc2.id
+
+  tags = {
+    Name = "Route Table for VPC 2"
+  }
+}
+
+# VPC Peering Connection
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  vpc_id        = aws_vpc.vpc1.id
+  peer_vpc_id   = aws_vpc.vpc2.id
+  auto_accept   = true  # Auto-accepts peering if within the same account
+
+  tags = {
+    Name = "VPC Peering Connection between VPC 1 and VPC 2"
+  }
+}
+
+# Route for VPC 1 to access VPC 2 via VPC Peering
+resource "aws_route" "route_to_vpc2" {
+  route_table_id            = aws_route_table.rt_vpc1.id
+  destination_cidr_block    = var.vpc2_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+}
+
+# Route for VPC 2 to access VPC 1 via VPC Peering
+resource "aws_route" "route_to_vpc1" {
+  route_table_id            = aws_route_table.rt_vpc2.id
+  destination_cidr_block    = var.vpc1_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
 }
--- vpc-peering/variables.tf
+++ vpc-peering/variables.tf
@@ -4,50 +4,14 @@ variable "aws_region" {
   default     = "eu-north-1"
 }
 
-variable "vpc_id" {
-  description = "VPC ID of the first VPC"
+variable "vpc1_cidr" {
+  description = "CIDR block for VPC 1"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "peer_vpc_id" {
-  description = "VPC ID of the peer VPC"
+variable "vpc2_cidr" {
+  description = "CIDR block for VPC 2"
   type        = string
-}
-
-variable "peer_owner_id" {
-  description = "Account ID of the peer VPC owner (if different account)"
-  type        = string
-  default     = ""
-}
-
-variable "auto_accept" {
-  description = "Auto-accept the VPC peering connection if in the same account"
-  type        = bool
-  default     = true
-}
-
-variable "peering_name" {
-  description = "Name for the VPC peering connection"
-  type        = string
-  default     = "vpc-peering"
-}
-
-variable "route_table_id_vpc1" {
-  description = "Route table ID for the first VPC"
-  type        = string
-}
-
-variable "route_table_id_vpc2" {
-  description = "Route table ID for the peer VPC"
-  type        = string
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block of the first VPC"
-  type        = string
-}
-
-variable "peer_vpc_cidr" {
-  description = "CIDR block of the peer VPC"
-  type        = string
+  default     = "10.1.0.0/16"
 }
