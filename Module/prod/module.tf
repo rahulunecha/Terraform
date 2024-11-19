44e54efa821e55aa721a1b5ea674244013f3f9a5
Ra}{ul Unecha <runecha08@gmail.com>
2024-11-19 12:33:08 +0530

    dd

--- Module/prod/module.tf
+++ Module/prod/module.tf
@@ -31,10 +31,10 @@ output "instance_public_ip" {
 }
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = module.this_vpc.vpc_id
+  value       = aws_vpc.this_vpc.vpc_id
 }
 
 output "subnet_id" {
   description = "The ID of the Subnet"
-  value       = module.this_subnet_pub.subnet_id
+  value       = aws_subnet.this_subnet_pub.subnet_id
 }
