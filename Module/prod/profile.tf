0adf99442eb378c216fedc3b9067cdb5ca4ae73e
Ra}{ul Unecha <runecha08@gmail.com>
2024-11-19 12:36:05 +0530

    x

--- Module/prod/profile.tf
+++ Module/prod/profile.tf
@@ -1,12 +1,3 @@
-
 provider "aws" {
-   profile = "configs"
-    default_tags {
-                tags = {
-                    name = "aws"
-                }
-    }
+  region = "eu-north-1"
 }
-#provider "aws" {
-#  region = "eu-north-1"
-#}
