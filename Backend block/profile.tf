12375216f28198a9f2a8164a9ba510f3714eab3f
Ra}{ul Unecha <runecha08@gmail.com>
2024-11-21 11:15:39 +0530

    dd

--- Backend block/profile.tf
+++ Backend block/profile.tf
@@ -1,6 +1,6 @@
 provider "aws" {
       region = "us-east-1"
       profile = "configs"
-      shared_credentials_files = ["/home/anup/.aws/credentials"]
+      shared_credentials_files = ["/home/rahul/.aws/credentials"]
     
 }
